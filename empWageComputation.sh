413925d7a812d9315019ad05e67bed2b904d5d33
sravanichinna <sravanidane413.com>
2020-11-21 12:13:03 +0530

    Added: EmployeePart/Full-Time logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-isPresent=1
-random=$(( RANDOM%2 ))
-echo "Random number: "$random
-if(( $random == $isPresent ))
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+random=$(( RANDOM%3 ))
+echo "Random number: " $random
+if(( $random == $IS_PART_TIME ))
 then
-	echo "Employee is Present!"
+	empHrs=4
+elif(( $random == $IS_FULL_TIME))
+then
 	empHrs=8
-	ratePerHour=20
-	salary=$(( $empHrs * $ratePerHour))
 else
-	echo "Employee is absent!"
-	salary=0
+	empHrs=0
 fi
+echo "Employee hours: " $empHrs
+salary=$(( $empHrs * $ EMP_RATE_PER_HR ))
 
 echo "Salary: $salary"
