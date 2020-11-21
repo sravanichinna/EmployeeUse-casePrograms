28841ee8399269c4d8c48e43caa51bf609dfd109
sravanichinna <sravanidane413.com>
2020-11-21 12:25:24 +0530

    Added: EmployeeSalaryForMonth logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,15 +2,21 @@
 IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HR=20
-random=$(( RANDOM%3 ))
-echo "Random number: $random"
-case $random in
-	$IS_PART_TIME) empHrs=4
-		;;
-	$IS_FULL_TIME) empHrs=8
-		;;
-	*) empHrs=0
-esac
-echo "Employee hours: $empHrs"
-salary=$(( $empHrs * $EMP_RATE_PER_HR ))
-echo "Salary: $salary"
+NUM_WORKING_DAYS=20
+totalSalary=0
+for(( day=1 ; day <= $NUM_WORKING_DAYS; day++ ))
+do
+	random=$(( RANDOM%3 ))
+	case $random in
+		$IS_PART_TIME) empHrs=4
+			;;
+		$IS_FULL_TIME) empHrs=8
+			;;
+		*) empHrs=0
+	esac
+	salary=$(( $empHrs * $ EMP_RATE_PER_HR))
+	echo "Day: $day Salary: $salary"
+	totalSalary=$(( $totalSalary + $salary ))
+done
+
+echo "Total Salary: $totalSalary"
