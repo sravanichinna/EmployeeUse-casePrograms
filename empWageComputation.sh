dcc1e8cb4a0042f78f042e74511f266f8326935f
sravanichinna <sravanidane413.com>
2020-11-21 12:18:19 +0530

    Added: EmployeeWorkingHours logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,17 +3,14 @@ IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HR=20
 random=$(( RANDOM%3 ))
-echo "Random number: " $random
-if(( $random == $IS_PART_TIME ))
-then
-	empHrs=4
-elif(( $random == $IS_FULL_TIME))
-then
-	empHrs=8
-else
-	empHrs=0
-fi
-echo "Employee hours: " $empHrs
-salary=$(( $empHrs * $ EMP_RATE_PER_HR ))
-
+echo "Random number: $random"
+case $random in
+	$IS_PART_TIME) empHrs=4
+		;;
+	$IS_FULL_TIME) empHrs=8
+		;;
+	*) empHrs=0
+esac
+echo "Employee hours: $empHrs"
+salary=$(( $empHrs * $EMP_RATE_PER_HR ))
 echo "Salary: $salary"
